676085ec730493673c10803055046299a0248806
Hugo Tamm <hugotamm@hotmail.com>
2023-06-19 01:31:30 +0300

    Added stuff to database tab

--- src/Tabs/Database.tsx
+++ src/Tabs/Database.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import 'Styles/Database.scss'
 
 function Database() {
   return (
     <div className="content database">
-        <h1>Database 🚧</h1>
+        <h1>Database</h1>
+        <div className="database-select">
+          <p className="text">Current database:</p>
+          <p className="current">None</p>
+          <div>
+            <button>Change</button>
+            <button>Remove</button>
+          </div>
+        </div>
     </div>
   )
 }
