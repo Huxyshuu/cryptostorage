65b7a4240dd6ad3a29eaf3f79bf69de29800882b
Hugo Tamm <hugotamm@hotmail.com>
2023-06-21 19:15:30 +0300

    current database text changing accordingly

--- src/Scripts/db_functions.tsx
+++ src/Scripts/db_functions.tsx
@@ -1,53 +1,49 @@
 const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
 
-export default class Database {
-    
+let currentDatabase = "None";
 
-    static addDatabase(event: Event): void {
-        const ev = event.target.files[0];
-        if (event) {
-            if (fs.existsSync(`./src/Database/${ev.name}`)) {
-                console.log("The file already exists. Please choose a different file.");
-            } else {
-                fs.copyFile(ev.path, `./src/Database/${ev.name}`, (err) => {
-                    if (err) {
-                        console.log(err.message);
-                    }
-                    else {
-                        currentDatabase = `./src/Database/${ev.name}`
-                        console.log("done!");
-                    }
-                });
-            }
+export function addDatabase(event: Event): void {
+    const ev = event.target.files[0];
+    if (event && ev) {
+        if (fs.existsSync(`./src/Database/${ev.name}`)) {
+            console.log("The file already exists. Please choose a different file.");
+        } else {
+            fs.copyFile(ev.path, `./src/Database/${ev.name}`, (err) => {
+                if (err) {
+                    console.log(err.message);
+                }
+                else {
+                    currentDatabase = `./src/Database/${ev.name}`
+                    document.getElementById("currentDatabase").innerHTML = ev.name;
+                }
+            });
         }
     }
-
-    static changeDatabase(event: Event): void {
-        return
-    }
-
-    static removeCurrentDatabase(): void {
-        console.log(currentDatabase)
-    }
-
-    createDatabase(name: string) {
-        const db = new sqlite3.Database('./src/Database/database1.db',
-            (err) => {
-                if (err) return console.error(err.message);
-            });
-    }
-
-    createTable() {
-        let sql = 'CREATE TABLE transactions(id INTEGER PRIMARY KEY, coin, short, value, amount, date, taxed)';
-        db.run(sql);
-    }
-
-    
 }
 
+export function changeDatabase(event: Event): void {
+    const ev = event.target.files[0];
+    if (event && ev) {
+        currentDatabase = `./src/Database/${ev.name}`
+        document.getElementById("currentDatabase").innerHTML = ev.name;
+    }
+}
 
+export function removeCurrentDatabase(): void {
+    currentDatabase = "None";
+    document.getElementById("currentDatabase").innerHTML = "None";
+}
 
+export function createDatabase(name: string) {
+    const db = new sqlite3.Database('./src/Database/database1.db',
+        (err) => {
+            if (err) return console.error(err.message);
+        });
+}
 
-//create table
+export function createTable() {
+    const sql = 'CREATE TABLE transactions(id INTEGER PRIMARY KEY, coin, short, value, amount, date, taxed)';
+    db.run(sql);
+}
 
--- src/Tabs/Database.tsx
+++ src/Tabs/Database.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'Styles/Database.scss'
-import DB from '../Scripts/db_functions.tsx';
+import {addDatabase, changeDatabase, removeCurrentDatabase} from '../Scripts/db_functions.tsx';
 
 function Database() {
 
@@ -9,15 +9,15 @@ function Database() {
         <h1>Database</h1>
         <div className="database-current">
           <p className="text">Current database:</p>
-          <p className="current">None</p>
+          <p className="current" id="currentDatabase">None</p>
         </div>
         <div className="database-button">
           <button onClick={() => console.log("Create New")}>Create New</button>
           <label htmlFor="database-file-add">Add Existing</label>
-          <input onChange={DB.addDatabase} type="file" id="database-file-add" name="database-file-add" accept=".db"/>
+          <input onChange={addDatabase} type="file" id="database-file-add" name="database-file-add" accept=".db"/>
           <label htmlFor="database-file-change">Change</label>
-          <input onChange={DB.changeDatabase} type="file" id="database-file-change" name="database-file-change" accept=".db"/>
-          <button onClick={() => DB.removeCurrentDatabase()}>Remove</button>
+          <input onChange={changeDatabase} type="file" id="database-file-change" name="database-file-change" accept=".db"/>
+          <button onClick={() => removeCurrentDatabase()}>Remove</button>
         </div>
     </div>
   )
