ba07870e9bd828468db6d0270193c421ebc95e4c
Hugo Tamm <hugotamm@hotmail.com>
2023-06-21 18:46:46 +0300

    Implementing database functions as a class?

--- src/Scripts/db_functions.tsx
+++ src/Scripts/db_functions.tsx
@@ -2,15 +2,36 @@ const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
 
 export default class Database {
+    
+
     static addDatabase(event: Event): void {
-        console.log(event.target.files[0].path)
+        const ev = event.target.files[0];
+        if (event) {
+            if (fs.existsSync(`./src/Database/${ev.name}`)) {
+                console.log("The file already exists. Please choose a different file.");
+            } else {
+                fs.copyFile(ev.path, `./src/Database/${ev.name}`, (err) => {
+                    if (err) {
+                        console.log(err.message);
+                    }
+                    else {
+                        currentDatabase = `./src/Database/${ev.name}`
+                        console.log("done!");
+                    }
+                });
+            }
+        }
     }
 
     static changeDatabase(event: Event): void {
-        console.log(event.target.files[0].name)
+        return
     }
 
-    createDatabase() {
+    static removeCurrentDatabase(): void {
+        console.log(currentDatabase)
+    }
+
+    createDatabase(name: string) {
         const db = new sqlite3.Database('./src/Database/database1.db',
             (err) => {
                 if (err) return console.error(err.message);
@@ -21,6 +42,8 @@ export default class Database {
         let sql = 'CREATE TABLE transactions(id INTEGER PRIMARY KEY, coin, short, value, amount, date, taxed)';
         db.run(sql);
     }
+
+    
 }
 
 
--- src/Tabs/Database.tsx
+++ src/Tabs/Database.tsx
@@ -13,13 +13,11 @@ function Database() {
         </div>
         <div className="database-button">
           <button onClick={() => console.log("Create New")}>Create New</button>
-          {/* <button onClick={() => console.log("Add Existing")}>Add Existing</button>
-          <button onClick={() => console.log("Change")}>Change</button> */}
           <label htmlFor="database-file-add">Add Existing</label>
           <input onChange={DB.addDatabase} type="file" id="database-file-add" name="database-file-add" accept=".db"/>
           <label htmlFor="database-file-change">Change</label>
           <input onChange={DB.changeDatabase} type="file" id="database-file-change" name="database-file-change" accept=".db"/>
-          <button onClick={() => console.log("Remove")}>Remove</button>
+          <button onClick={() => DB.removeCurrentDatabase()}>Remove</button>
         </div>
     </div>
   )
