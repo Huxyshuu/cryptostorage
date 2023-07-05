b899681a9d2f777bb9ac03846a4e6e83ded70570
Hugo Tamm <hugotamm@hotmail.com>
2023-07-05 15:43:59 +0300

    added functionality to create new database button

--- src/Tabs/Database.tsx
+++ src/Tabs/Database.tsx
@@ -1,8 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import 'Styles/Database.scss'
 import {addDatabase, changeDatabase, removeCurrentDatabase, saveData, loadData} from '../Scripts/db_functions.tsx';
 
 function Database() {
+
+  const [creatingNew, setCreatingNew] = useState(false);
+
+  const createNew = () => {
+    setCreatingNew(false);
+  }
   
   useEffect(() => {
     loadData();
@@ -11,12 +17,21 @@ function Database() {
   return (
     <div className="content database">
         <h1>Database</h1>
+        { creatingNew ? 
+        <div className="database-current">
+          <p className="text">New database name:</p>
+          <input type="text" />
+          <input type="submit" value="Create" onClick={createNew}/>
+        </div>
+        :
         <div className="database-current">
           <p className="text">Current database:</p>
           <p className="current" id="currentDatabase">None</p>
         </div>
+        }
+        
         <div className="database-button">
-          <button onClick={() => console.log("Create New")}>Create New</button>
+          <button onClick={() => setCreatingNew(true)}>Create New</button>
           <label htmlFor="database-file-add">Add Existing</label>
           <input onChange={addDatabase} type="file" id="database-file-add" name="database-file-add" accept=".db"/>
           <label htmlFor="database-file-change">Change</label>
